f4504b3bcd99b6e0cc9c6c4d17ff580d7b05acd9
Yahya9102 <yaya_hussein@hotmail.com>
2025-11-20 07:37:00 +0100

    minor changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SignupForm from './components/SignupForm'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const secret = import.env.VITE_MYSECRITVARIABLE
+  
   return (
     <>
        <div>
+        <p>{secret}</p>
       <h1>DevOps Demo</h1>
       <h2> Ni ska sätta upp CI/CD för.
         Pipen ska köra tester och bygga projektet innan deploy.</h2>
