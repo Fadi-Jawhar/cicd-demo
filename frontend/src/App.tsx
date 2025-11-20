bb89878932d3f902b61c77d1b15b29af806d7e06
Fadi-Jawhar <fadi_jawhar@hotmail.com>
2025-11-20 10:35:02 +0100

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const secret = import.env.VITE_MYSECRITVARIABLE
+  const secret = import.meta.env.VITE_MYSECRITVARIABLE
   
   return (
     <>
